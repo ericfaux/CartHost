e5055663844ffd2064130086b5853b1dfac3cf46
ericfaux <67875487+ericfaux@users.noreply.github.com>
2026-01-01 11:37:12 -0500

    Configure remote image patterns for Supabase
    Added remote image patterns for Supabase storage.

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co', // Covers all Supabase projects
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/sign/**', // Important for Signed URLs
+      },
+    ],
+  },
+};
 
 export default nextConfig;
